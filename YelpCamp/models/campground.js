0d842302ab974b8f93c3fea26e5b42cd1fad9c27
xbenxspire <benpierce94@gmail.com>
2024-07-07 12:27:29 -0700

    Updated with error handling, middleware, and deleting reviews

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = mongoose.model('Review');
 
 const CampgroundSchema = new Schema({
-    title: String,
-    image: String,
-    price: Number,
-    description: String,
-    location: String
+    title: { type: String, required: true },
+    image: { type: String, required: true },
+    price: { type: Number, required: true, min: 0 },
+    description: { type: String, required: true },
+    location: { type: String, required: true },
+    reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }]
+});
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({ _id: { $in: doc.reviews } });
+    }
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
